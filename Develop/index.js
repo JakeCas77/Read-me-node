09b55024487dd8efaec647970198d06bd5df85a5
JakeCas77 <jakedac@gmail.com>
2023-06-20 18:26:53 -0500

    Added source code/Added input questions

---
+++ Develop/index.js
@@ -0,0 +1,59 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Description"
+}, {
+    type:"input",
+    message: "Table of Contents.",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What does the user need to install to run this app (ie...steam)",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "How is the app used? Give instructions",
+    name: "Usage"
+}, {
+    type: "input",
+    message: "Who contributed to this project?:",
+    name: "Contributing"
+}, {
+    type: "input",
+    message: "What license is being used? (ie...MIT)",
+    name: "License"
+}, {
+    type: "input",
+    message: "What commands are needed to test this app?",
+    name: "Tests"
+}, {
+    type: "input",
+    message: "Contact info for inquiries.",
+    name: "Questions"
+}, {
+    type: "input",
+    message: "What is your Github username?",
+    name: "Username"
+}, {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email"
+},
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
